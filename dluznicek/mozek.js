285b998c56e3a17e0c5256cc81219e545247103c
Krata91 <kratochvilejan@gmail.com>
2023-03-21 22:17:20 +0100

    Update mozek.js

--- dluznicek/mozek.js
+++ dluznicek/mozek.js
@@ -14,7 +14,35 @@ function vykresliSeznam() {
 	seznam.innerHTML = "";
 	for (var i = 0; i < uzivatele.length; i++) {
 		var polozka = document.createElement("li");
-		polozka.appendChild(document.createTextNode(uzivatele[i]));
+		var jmeno = document.createElement("span");
+		jmeno.appendChild(document.createTextNode(uzivatele[i].jmeno));
+		polozka.appendChild(jmeno);
+		var castka = document.createElement("input");
+		castka.type = "number";
+		castka.value = uzivatele[i].castka;
+		castka.addEventListener("change", function() {
+			uzivatele[i].castka = parseInt(this.value);
+			vypocitejCelkovouCastku();
+		});
+		polozka.appendChild(castka);
 		seznam.appendChild(polozka);
 	}
 }
+
+
+function vypocitejCelkovouCastku() {
+	var celkovaCastka = 0;
+	for (var i = 0; i < uzivatele.length; i++) {
+		celkovaCastka += uzivatele[i].castka;
+	}
+	document.getElementById("celkova-castka").textContent = celkovaCastka;
+}
+
+
+function inicializuj() {
+	vykresliSeznam();
+	vypocitejCelkovouCastku();
+}
+
+
+window.onload = inicializuj;
